7ad557df61361ee811e268fa1a6c74b05fc311bd
DMon18 <101190737@student.swin.edu.au>
2017-05-25 02:13:40 +1000

    20% discount allow on first order

--- getadd.php
+++ getadd.php
@@ -1,5 +1,5 @@
 <?php
-  
+  session_start();
   /**
   * Author: Guillaume Dinys Monvoisin 101190737
   * Target: register.php
@@ -8,16 +8,13 @@
   * Last updated: 29/03/2017
   * Credits:
   */  
-
-  require_once("settings.php"); // connection info
   
-  $conn = @mysqli_connect($host,
-    $user,
-    $pwd,
-    $sql_db
-  );
+    // Open the Database connection
+  $DBConnection = @mysqli_connect("feenix-mariadb.swin.edu.au", "s100864950","120897", "s100864950_db")
+  Or die ("<p>Unable to connect to the database server.</p>". "<p>Error code ". 
+  mysqli_connect_errno().": ". mysqli_connect_error()). "</p>"; 
 
-  if (!$conn) 
+  if (!$DBConnection) 
   {
     // Displays an error message
     echo "<p>Database connection failure</p>"; // not in production script
@@ -25,62 +22,96 @@
   else
   {
     //check if data is input from the form and not url use to get to the php site
-    if (isset($_POST['submitbutton']) && isset($_POST['fname']))
+    if (isset($_POST['submitbutton']))
     {
-      if (isset($_POST['fname'])) $fname = $_POST['fname'];
-      if (isset($_POST['Mobile_Number'])) $Mobile_Number = $_POST['Mobile_Number'];
-      if (isset($_POST['emailContact'])) $emailContact = $_POST['emailContact'];
-      if (isset($_POST['cbSunshine'])) $cbSunshine = $_POST['cbSunshine'];
-      if (isset($_POST['cbHAwthorn'])) $cbHAwthorn = $_POST['cbHAwthorn'];
-      if (isset($_POST['cbRichmond'])) $cbRichmond = $_POST['cbRichmond'];
-      if (isset($_POST['cbCollingwood'])) $cbCollingwood = $_POST['cbCollingwood'];
-      if (isset($_POST['cbParkdale'])) $cbParkdale = $_POST['cbParkdale'];
-      if (isset($_POST['cbKew'])) $cbKew = $_POST['cbKew'];
-      if (isset($_POST['cbFootscray'])) $cbFootscray = $_POST['cbFootscray'];
-      if (isset($_POST['cbBruno'])) $cbBruno = $_POST['cbBruno'];
-      if (isset($_POST['quantity1'])) $quantity1 = $_POST['quantity1'];
-      if (isset($_POST['quantity1'])) $quantity2 = $_POST['quantity2'];
-      if (isset($_POST['quantity1'])) $quantity3 = $_POST['quantity3'];
-      if (isset($_POST['quantity1'])) $quantity4 = $_POST['quantity4'];
-      if (isset($_POST['quantity1'])) $quantity5 = $_POST['quantity5'];
-      if (isset($_POST['quantity1'])) $quantity6 = $_POST['quantity6'];
-      if (isset($_POST['quantity1'])) $quantity7 = $_POST['quantity7'];
-      if (isset($_POST['quantity1'])) $quantity8 = $_POST['quantity8'];
-      if (isset($_POST['unithouse'])) $unithouse = $_POST['unithouse'];
-      if (isset($_POST['streetname'])) $streetname = $_POST['streetname'];
-      if (isset($_POST['citydel'])) $citydel = $_POST['citydel'];
-      if (isset($_POST['statename'])) $state = $_POST['statename'];
-      if (isset($_POST['postdel'])) $postdel = $_POST['postdel'];
+    if (isset($_POST['quantity1'])) $quantity1 = $_POST['quantity1'];
+    if (isset($_POST['quantity1'])) $quantity2 = $_POST['quantity2'];
+    if (isset($_POST['quantity1'])) $quantity3 = $_POST['quantity3'];
+    if (isset($_POST['quantity1'])) $quantity4 = $_POST['quantity4'];
+    if (isset($_POST['quantity1'])) $quantity5 = $_POST['quantity5'];
+    if (isset($_POST['quantity1'])) $quantity6 = $_POST['quantity6'];
+    if (isset($_POST['quantity1'])) $quantity7 = $_POST['quantity7'];
+    if (isset($_POST['quantity1'])) $quantity8 = $_POST['quantity8'];
+    if (isset($_POST['unithouse'])) $unithouse = $_POST['unithouse'];
+    if (isset($_POST['streetname'])) $streetname = $_POST['streetname'];
+    if (isset($_POST['citydel'])) $citydel = $_POST['citydel'];
+    if (isset($_POST['statename'])) $state = $_POST['statename'];
+    if (isset($_POST['postdel'])) $postdel = $_POST['postdel'];
+    if (isset($_POST['method_of_payment'])) $paymenttype = $_POST['method_of_payment'];
+    if (isset($_POST['cardtype'])) $cardtype = $_POST['cardtype'];
+    if (isset($_POST['name_on_card'])) $name_card = $_POST['name_on_card'];
+    if (isset($_POST['card_Number'])) $card_num = $_POST['card_Number'];
+    if (isset($_POST['ValidTill'])) $validtill = $_POST['ValidTill'];
+    if (isset($_POST['Total_Bill'])) $total_bill = $_POST['Total_Bill'];
     
 
-      $arr = array($quantity1, $quantity2, $quantity3,$quantity4,$quantity5,$quantity6,$quantity7,$quantity8);
+        $arr = array($quantity1, $quantity2, $quantity3,$quantity4,$quantity5,$quantity6,$quantity7,$quantity8);
+        checkQuantity($arr);
+    $customerid = $_SESSION['custid'];
       
-      checkQuantity($arr);
-      //insert the inputs entered by customer and generated number
-      $queryInsert = "INSERT INTO `ABSOrder` VALUES ('$fname','$Mobile_Number','$emailContact',$quantity1,$quantity2,$quantity3,$quantity4,$quantity5,$quantity6,$quantity7,$quantity8,'$unithouse','$streetname','$citydel','$state', $postdel, 0)";
-
-      $resultInsert = mysqli_query($conn, $queryInsert);
-
-      //checks if the execution was successful
-      if (!$resultInsert)
-      {
-        echo "<p>Something is wrong with ", $queryInsert, "</p>";
-      }
-      else
-      {
-          echo "<p>Dear {$fname}, you have successfully ordered.<p>";
-      }
+    $SQLstring1 = "select * from ABSOrder where cnumber = '".$customerid."'";
+    $queryResult1 = @mysqli_query($DBConnection, $SQLstring1);
+    $rowcount1 = mysqli_num_rows($queryResult1);
+    
+    $SQLstring = "select * from ABSOrder";
+    $queryResult = @mysqli_query($DBConnection, $SQLstring);
+    $rowcount = mysqli_num_rows($queryResult);
+    
+    if ($rowcount1 == 0)
+    {
+      $assignOrder = $rowcount + 5000;
+      $total1 = calculateTotal($arr)*0.8;
+      
+      // Set up of SQL strings and Execution
+      $Query = "insert into ABSOrder values ('$customerid', '$assignOrder', '$arr[0]', '$arr[1]', '$arr[2]', '$arr[3]', '$arr[4]', '$arr[5]', '$arr[6]', '$arr[7]', '$total1', '$unithouse', '$streetname', '$citydel', '$state', '$postdel', '$paymenttype', '$cardtype', '$name_card', '$card_num', '$validtill')";
+      $Results = mysqli_query($DBConnection, $Query);
+      
+      $message = "Thank You!! Your order request has been successfully placed and your order number is $assignOrder";
+      echo "<script type='text/javascript'>alert('$message');</script>";
+    
+      // Closing the Database Connection
+      mysqli_close($DBConnection);
+      include 'order.php';
     }
-  }
+    else
+    {
+      $assignOrder = $rowcount + 5000;
+      $total2 = calculateTotal($arr);
+      
+      // Set up of SQL strings and Execution
+      $Query = "insert into ABSOrder values ('$customerid', '$assignOrder', '$arr[0]', '$arr[1]', '$arr[2]', '$arr[3]', '$arr[4]', '$arr[5]', '$arr[6]', '$arr[7]', '$total2', '$unithouse', '$streetname', '$citydel', '$state', '$postdel', '$paymenttype', '$cardtype', '$name_card', '$card_num', '$validtill')";
+      $Results = mysqli_query($DBConnection, $Query);
+      
+      $message = "Thank You!! Your order request has been successfully placed and your order number is $assignOrder";
+      echo "<script type='text/javascript'>alert('$message');</script>";
+    
+      // Closing the Database Connection
+      mysqli_close($DBConnection);
+      include 'order.php';
+    }
+    }
+}
 
 function checkQuantity($arr)
 {
-  for ($i=1; $i < 8; $i++) {
-      if($arr[$i] == "")
-      {
-        $arr[$i] = 0;
-      }
+  for ($i=1; $i < 8; $i++)
+  {
+    if($arr[$i] == "")
+        {
+      $arr[$i] = 0;
+        }
     }
+}
+
+function calculateTotal($arr)
+{
+  $a = 0.00;
+  $price = [13.60,13.85,13.50,14.10,13.90,14.55,12.90,15.00];
+  for ($i=0; $i<8; $i++)  /*This function calculates the total bill of the customer*/
+  {             
+    $a += $arr[$i]*$price[$i];
+  }
+  return $a;
 }
 
 ?>
--- order.php
+++ order.php
@@ -1,123 +1,143 @@
-<?php
-    session_start();
-    if (!isset($_SESSION['verify']))
-        header("location: error.htm");
-?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-	<meta charset="utf-8" />
-	<meta name="description" content="Web development" />
+	<meta charset="utf-8" />	
+	<meta name="description" content="Development Project 1" />
 	<meta name="keywords"    content="Registration Form" />
-	<meta name="author"      content="DP1 Team" />
+	<meta name="author"      content="Dinys Monvoisin" />
 	<link href="css/style.css" rel="stylesheet" type="text/css" />
   	
-	<title>ABS | Order</title>
+	<title>Order</title>
 
-	<script src="JavaScript/order.js"></script>
+	<script src="order.js"></script>
 </head>
 <body> <!--This is the Order page of Bruno Pizzeria from which the customers can order the pizza online-->
-<header>
-<h1>ORDER BURGER ONLINE</h1>
-</header>
-<nav>
-  <ul>
-    <li><a href="index.html">HOME</a></li>
-	<li><a href="about.html">ABOUT US</a></li>
-	<li><a href="registration.php">SIGN UP</a></li>
-	<li><a class="active" href="order.php">ORDER BURGER</a></li>
-      <li><a href="logout.php">LOG OUT</a></li>
-  </ul>	 
-</nav>
-<main>
-<h2>ORDER YOUR BURGER ONLINE</h2>
-<p class="intro">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Now you can ORDER your burger online by filling out this simple form and we will be ready with your burger within 20 minutes. Please follow all the instructions provided by the page while you are filling out the form. <strong>THANK YOU!!!</strong>.</p>
-<form id="order" method="post" action="getadd.php">
-          <fieldset class="field1">
-		  <legend class="info1">Contact Details:</legend>
-		  <div class="name">
-    <label>Customer Name:-</label>
-<input id="v2" type="text" name="fname" />
- </div>
- <div class="mobile">
- <label>Mobile Number:-</label>
-<input id="v1" type="text" name="Mobile_Number" maxlength="12" size="12" />
- </div>
- <div class="email"><label>Email Address for receipt:-</label>
-<input type="email" name="emailContact" placeholder="email@domain.com" />
-</div>
+	<header>
+	<h1>ORDER BURGER ONLINE</h1>
+	</header>
+	<nav>
+	  <ul>
+		<li><a href="home.php">HOME</a></li>
+		<li><a href="about.php">ABOUT US</a></li>
+		<li><a href="registration.php">SIGN UP</a></li>
+		<li><a href="login.php">CUSTOMER LOGIN</a></li>
+		<li><a class="active" href="order.php">ORDER ONLINE</a></li>
+		<li><a href="feedback.php">FEEDBACK</a></li>
+		<li><a href="adminlogin.php">ADMIN LOGIN</a></li>
+		<li><a href="logout.php">LOGOUT</a></li>
+	  </ul>	 
+	</nav>
+	<main>
+		<h2>ORDER YOUR BURGER ONLINE</h2>
+		<p class="intro">Now you can ORDER your burger online by filling out this simple form and we will be ready with your burger within 20 minutes. Please follow all the instructions provided by the page while you are filling out the form. <strong>THANK YOU!!!</strong>.</p>
+		<form id="order" method="post" action="getadd.php">
 
-<fieldset class="field7">
-  <legend class="info7">Choose the burger and the quantity you want to order :</legend>
-  <label class="imp"><strong>Note:- If you will not assign any value to quantity, then the ORDER for that particular burger will not be considered. And once you are done selecting the pizzas with corresponding quantity, Press OK to see your total BILL.</strong></label> <br/><br/>
- <input type="checkbox" name="cbSunshine"
-value="Sunshine Beef and Fries" id="sun" />Sunshine Beef and Fries   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity1" size="10" maxlength="10" placeholder="QUANTITY" id="q1" /> <br/>
-<input type="checkbox" name="cbHAwthorn"
-value="Hawthorn Ultimate Chicken" id="haw" />Hawthorn Ultimate Chicken    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity2" size="10" maxlength="10" placeholder="QUANTITY" id="q2"/><br/>
-<input type="checkbox" name="cbRichmond"
-value="Richmond Beef Double" id="rich" />Richmond Beef Double    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity3" size="10" maxlength="10" placeholder="QUANTITY" id="q3"/><br/>
-<input type="checkbox" name="cbCollingwood"
-value="Collingwood Grilled Chicken" id="col" />Collingwood Grilled Chicken    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity4" size="10" maxlength="10" placeholder="QUANTITY" id="q4"/><br/>
-<input type="checkbox" name="cbParkdale"
-value="Parkdale Crispy Chicken" id="park" />Parkdale Crispy Chicken    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity5" size="10" maxlength="10" placeholder="QUANTITY" id="q5"/><br/>
-<input type="checkbox" name="cbKew"
-value="Parkdale Crispy Chicken" id="kew" />Kew Classic Angus       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity6" size="10" maxlength="10" placeholder="QUANTITY" id="q6"/><br/>
-<input type="checkbox" name="cbFootscray"
-value="Footscray Fish Burger" id="foot" />Footscray Fish Burger     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity7" size="10" maxlength="10" placeholder="QUANTITY" id="q7"/><br/>
-<input type="checkbox" name="cbBruno"
-value="ABS Special Burger" id="bruno" />ABS Special Burger    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity8" size="10" maxlength="10" placeholder="QUANTITY" id="q8"/><br/><br/>
+		<fieldset class="field7">
+		  <legend class="info7">Choose the burger and the quantity you want to order :</legend>
+		  <label class="imp"><strong>Note:- If you will not assign any value to quantity, then the ORDER for that particular burger will not be considered. And once you are done selecting the pizzas with corresponding quantity, Press OK to see your total BILL.</strong></label> <br/><br/>
+		 <input type="checkbox" name="cbSunshine"
+		value="Sunshine Beef and Fries" id="sun" />Sunshine Beef and Fries   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity1" size="10" maxlength="10" placeholder="QUANTITY" id="q1" /> <br/>
+		<input type="checkbox" name="cbHAwthorn"
+		value="Hawthorn Ultimate Chicken" id="haw" />Hawthorn Ultimate Chicken    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity2" size="10" maxlength="10" placeholder="QUANTITY" id="q2"/><br/>
+		<input type="checkbox" name="cbRichmond"
+		value="Richmond Beef Double" id="rich" />Richmond Beef Double    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity3" size="10" maxlength="10" placeholder="QUANTITY" id="q3"/><br/>
+		<input type="checkbox" name="cbCollingwood"
+		value="Collingwood Grilled Chicken" id="col" />Collingwood Grilled Chicken    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity4" size="10" maxlength="10" placeholder="QUANTITY" id="q4"/><br/>
+		<input type="checkbox" name="cbParkdale"
+		value="Parkdale Crispy Chicken" id="park" />Parkdale Crispy Chicken    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity5" size="10" maxlength="10" placeholder="QUANTITY" id="q5"/><br/>
+		<input type="checkbox" name="cbKew"
+		value="Parkdale Crispy Chicken" id="kew" />Kew Classic Angus       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity6" size="10" maxlength="10" placeholder="QUANTITY" id="q6"/><br/>
+		<input type="checkbox" name="cbFootscray"
+		value="Footscray Fish Burger" id="foot" />Footscray Fish Burger     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity7" size="10" maxlength="10" placeholder="QUANTITY" id="q7"/><br/>
+		<input type="checkbox" name="cbBruno"
+		value="ABS Special Burger" id="bruno" />ABS Special Burger    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="quantity8" size="10" maxlength="10" placeholder="QUANTITY" id="q8"/><br/><br/>
 
-<div style="text-align:center;"><a href="#" id="showBill" class="button">OK</a></div>
+		<div style="text-align:center;"><a href="#" id="showBill" class="button">OK</a></div>
 
-<div class="bill" id="total">
-<label><strong>Your total bill is:- </strong></label>  <input type="text" size="10" name="Total_Bill" maxlength="6" id="answer" />
-</div>
- </fieldset>
-	   
-		  <!--Delivery Address-->
-		  <fieldset class="field4">
-		  <legend class="info4">Delivery Address:</legend>
-			 
-		  <div class="houseno2">
-		  <label>Unit/House Number:-</label>
-		  <input type="text" name="unithouse" maxlength="5" size="5" id="houseno2" /> <br/></div>
-		  <div class="street2">
-		  <label>Street Name:-</label>
-		  <input type="text" name="streetname" maxlength="15" size="15" id="street2" /> <br/></div>
-		  <div class="city2">
-		   <label>City:-</label>
-		  <input type="text" name="citydel" maxlength="12" size="12" id="city2" /> <br/></div>
-		  <div class="state2">
-          <label>State:-</label>
-          
-			<select name="statename" id="state2">
-			<option value="">--Please Select--</option>
-			<option value="ACT">ACT</option>
-			<option value="NSW">NSW</option>
-			<option value="NT">NT</option>
-			<option value="QLD">QLD</option>
-			<option value="SA">SA</option>
-			<option value="TAS">TAS</option>
-			<option value="VIC">VIC</option>
-			<option value="WA">WA</option>
-			</select>
-		  </div>
+		<div class="bill" id="total">
+		<label><strong>Your total bill is:- </strong></label>  <input type="text" size="10" name="Total_Bill" maxlength="6" id="answer" />
+		</div>
+		 </fieldset>
+			   
+				  <!--Delivery Address-->
+				  <fieldset class="field4">
+				  <legend class="info4">Delivery Address:</legend>
+					 
+				  <div class="houseno2">
+				  <label>Unit/House Number:-</label>
+				  <input type="text" name="unithouse" maxlength="5" size="5" id="houseno2" /> <br/></div>
+				  <div class="street2">
+				  <label>Street Name:-</label>
+				  <input type="text" name="streetname" maxlength="15" size="15" id="street2" /> <br/></div>
+				  <div class="city2">
+				   <label>City:-</label>
+				  <input type="text" name="citydel" maxlength="12" size="12" id="city2" /> <br/></div>
+				  <div class="state2">
+				  <label>State:-</label>
+				  
+					<select name="statename" id="state2">
+					<option value="">--Please Select--</option>
+					<option value="ACT">ACT</option>
+					<option value="NSW">NSW</option>
+					<option value="NT">NT</option>
+					<option value="QLD">QLD</option>
+					<option value="SA">SA</option>
+					<option value="TAS">TAS</option>
+					<option value="VIC">VIC</option>
+					<option value="WA">WA</option>
+					</select>
+				  </div>
 
-		  <div class="postcode2">
-		  <label>Postcode:-</label>
-		  <input id="v3" type="text" name="postdel" maxlength="4" size="4"/> </div>
-		  </div>
+				  <div class="postcode2">
+				  <label>Postcode:-</label>
+				  <input id="v3" type="text" name="postdel" maxlength="4" size="4"/> </div>
+				  </div>
+				  </fieldset>
+				  
+				  <!--Payment-->
+				  <fieldset class="field5">
+				  <legend class="info5">Payment Type:</legend>
+				  <div class="method1">
+				  <input type="radio" name="method_of_payment" value="pay_on_PickUp" id="payPickup" checked="checked" />Pay on Pick Up <br/>
+				  <input type="radio" name="method_of_payment" value="Online" id="payonline" />Online Payment
+				  </div>
+				  </fieldset>
+				  
+				  
+				  
+				  <!--Card type-->
+				  <fieldset class="field6" id="onlinePayment">
+				  <legend class="info6">Online Payment:</legend>
+				  <div class="cardtype">
+				  <label>Card Type:-</label>
+				  <select name="cardtype" id="cardtype" >
+						<option value="">--Please Select--</option>
+						<option value="visa" id="visa">VISA</option>
+						<option value="master" id="master">MasterCard</option>
+						<option value="amex" id="amex">American Express</option>
+				</select></div>
+				<div class="cardname">
+				 <label>Name on Card:-</label>
+				 <input type="text" name="name_on_card" id="o1" /></div>
+				 <div class="cardnumber">
+				 <label>Card Number:-</label>
+				 <input type="text" name="card_Number" size="20" maxlength="15" id="cardNumber" /></div>
+				 <div class="validtill">
+				 <label>Valid Till:-</label>
+				 
+		  <input type="date" name="ValidTill" /></div>
 		  </fieldset>
-		  <p class="final">
-<input type="submit" value="Submit" name="submitbutton" />
-<input type="reset" value="Reset" />
-</p>
-</form>
-</main>
-<footer>
-<p>The contents of this website are protected by <strong>copyright&copy; law.</strong> Copyright in this material resides with the Commonwealth of Australia or various other rights holders, as indicated. Abhishek Mishra is the developer of this website as well the content provided in this website.</p>
-</footer>
+				  <p class="final">
+		<input type="submit" value="Order" name="submitbutton" />
+		<input type="reset" value="Reset" />
+		</p>
+		</form>
+	</main>
+	<footer>
+		<p>The contents of this website are protected by <strong>copyright&copy; law.</strong> Copyright in this material resides with the Commonwealth of Australia or various other rights holders, as indicated. DP1 - Group 1 is the developer of this website as well the content provided in this website.</p>
+	</footer>
 </body>
 </html>
-
-<?php include 'getadd.php';?>
+<?php
+session_start();
+?>
