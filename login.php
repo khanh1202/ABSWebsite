a5b12a6c80a0d9350d693b63ab16dc10cb8d2fb1
khanh1202 <dgkhanh97@gmail.com>
2017-05-25 02:23:50 +1000

    Log in/log out functions

--- login.php
+++ login.php
@@ -1,71 +1,92 @@
-<HTML XMLns="http://www.w3.org/1999/xHTML">
 <head>
-    <title>ABS | Login</title>
-    <meta charset="utf-8" />
-    <meta name="description" content="Web development" />
-    <meta name="keywords"    content="Login Form" />
-    <meta name="author"      content="DP1 Team" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
+	<meta charset="utf-8" />	
+	<meta name="description" content="Development Project 1" />
+	<meta name="keywords"    content="Registration Form" />
+	<meta name="author"      content="Abhishek Mishra" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+	<title>Customer Login</title>
 </head>
-<body>
-<header>
-    <h1>Bruno Pizerria Login Page</h1>
-</header>
-<nav>
-    <ul>
-        <li><a href="index.html">HOME</a></li>
-        <li><a href="about.html">ABOUT US</a></li>
-        <li><a href="registration.php">SIGN UP</a></li>
-        <li><a class="active" href="login.php">ORDER BURGER</a></li>
-        <li><a href="logout.php">LOG OUT</a></li>
-    </ul>
-</nav>
-
-<!--Form to check get the login details-->
-<main>
-    <h2>Login form for members</h2>
-<form id="order">
-    <fieldset class="field1">
-        <legend class="info1">Member Login</legend>
-        <p><label for="email">Email <input type="text" name="email" id="email"></label></p>
-        <p><label for="pass">Password: <input type="Password" name="pass" id="pass"></label></p>
-        <input type="submit" value="Log in" name="submit"/>
-        <p>Not a member? <a href="registration.php"> Sign up</a></p>
-    </fieldset>
-</form>
-</main>
+<body>       <!--This is the about page of Bruno Pizzeria which tells the history of Bruno Pizzeria-->
+	<header>
+	<h1>CUSTOMER LOGIN</h1>
+	</header>
+	<nav>
+	  <ul>
+		<li><a href="home.php">HOME</a></li>
+		<li><a href="about.php">ABOUT US</a></li>
+		<li><a href="registration.php">SIGN UP</a></li>
+		<li><a class="active" href="login.php">CUSTOMER LOGIN</a></li>
+		<li><a href="feedback.php">FEEDBACK</a></li>
+		<li><a href="adminlogin.php">ADMIN LOGIN</a></li>
+	  </ul>	 
+	</nav>
+	
+	<main>
+	<h2>LOGIN TO THE AUSTRALIAN BURGER STAR</h2>
+	<p class="intro">You can log into the system by entering your generated customer ID and password.</p>
+	<form method="post">
+	 <fieldset class="field1">
+	   <legend class="info1">Login Information:</legend>
+	 <div class="name">
+		<label>Customer Number:-</label>
+	<input id="r1" type="text" name="cnum" />
+	 </div>
+	 <div class="name">
+		<label>Password:-</label>
+		<input id="r2" type="password" name="pass" />
+	 </div>
+	 </fieldset>
+	
+	<p class="final">
+	<input type="submit" value="Login" />
+	<input type="reset" value="Reset" />
+	</p>
+	</form>
+	</main>
+	
+	<footer>
+		<p>The contents of this website are protected by <strong>copyright&copy; law.</strong> Copyright in this material resides with the Commonwealth of Australia or various other rights holders, as indicated. DP1 - Group 1 is the developer of this website as well the content provided in this website.</p>
+	</footer>
 </body>
-<?php
-if (isset($_GET['submit']))
-{
-    if (empty($_GET["email"]) || empty($_GET["pass"]))
-        echo "<p>Please fill all the fields</p>";
-    else
-    {
-        //get the information and connect to database
+</html>
 
-        if (!isset($_SESSION["verify"]))
-            $_SESSION["verify"] = false;
-        $email = $_GET["email"];
-        $pass = $_GET["pass"];
-        $user = "s101339475";
-        $pwd = "120297";
-        $database = "s101339475_db";
-        $dbConnect = @mysqli_connect("feenix-mariadb.swin.edu.au", $user, $pwd, $database) or die("<p>Unable to connect to the database server. </p>". "<p>Error code ". mysqli_connect_errno($dbConnect). ": ". mysqli_connect_error($dbConnect). "</p>");
-        //implement the query to check identity
-        $querystring1 = "select * from ABSCustomer where email='$email' and password='$pass'";
-        $query1 = @mysqli_query($dbConnect, $querystring1) or die ("<p>Unable to do the query 4</p>");
-        if ($rowquery1 = mysqli_fetch_assoc($query1))
-        {
-            session_start();
-            $_SESSION["verify"] = true;
-            header("Location: order.php");
-        }
-        else
-            echo "<p>Invalid email address or password</p>";
-        mysqli_free_result($query1);
-        mysqli_close($dbConnect);
-    }
-}
+<?php
+	session_start();
+	
+	if(isset($_POST['cnum']) && isset($_POST['pass']))
+	{
+		$cnum = $_POST['cnum'];
+		$pass = $_POST['pass'];
+		
+		if ($cnum == "" || $pass == "")
+		{
+			$errMsg = "Please make sure that none of the input fields are empty";
+			echo "<script type='text/javascript'>alert('$errMsg');</script>";
+		}
+		
+		else
+		{
+			// Open the Database connection
+			$DBConnection = @mysqli_connect("feenix-mariadb.swin.edu.au", "s100864950","120897", "s100864950_db")
+			Or die ("<p>Unable to connect to the database server.</p>". "<p>Error code ". 
+			mysqli_connect_errno().": ". mysqli_connect_error()). "</p>"; 
+			
+			$SQLstring = "select * from ABSCustomer where cno = '".$_POST['cnum']."' and cpassword = '".$_POST['pass']."'";
+			$queryResult = @mysqli_query($DBConnection, $SQLstring);
+			$rowcount = mysqli_num_rows($queryResult);
+			
+			if ($rowcount == 1)
+			{
+				$_SESSION['custid'] = $_POST['cnum'];
+				header('Location: order.php');
+			}
+			
+			else
+			{
+				$errMsg1 = "Please enter a valid Customer Number and Password";
+				echo "<script type='text/javascript'>alert('$errMsg1');</script>";
+			}
+		}
+	}
 ?>
-</HTML>
--- logout.php
+++ logout.php
@@ -1,6 +1,52 @@
-<?php
+<head>
+	<meta charset="utf-8" />	
+	<meta name="description" content="Development Project 1" />
+	<meta name="keywords"    content="Registration Form" />
+	<meta name="author"      content="Abhishek Mishra" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	
+	<script type="text/javascript">
+		function redirect()
+		{
+			window.location.href = 'home.php';
+		}
+	</script>
 
-session_start();
-session_destroy();
-header("location: login.php");
+	<title>Customer Login</title>
+</head>
+<body>       <!--This is the about page of Bruno Pizzeria which tells the history of Bruno Pizzeria-->
+	<header>
+	<h1>LOGOUT</h1>
+	</header>
+	<nav>
+	  <ul>
+		<li><a href="home.php">HOME</a></li>
+		<li><a href="about.php">ABOUT US</a></li>
+		<li><a href="registration.php">SIGN UP</a></li>
+		<li><a href="login.php">CUSTOMER LOGIN</a></li>
+		<li><a href="feedback.php">FEEDBACK</a></li>
+		<li><a href="adminlogin.php">ADMIN LOGIN</a></li>
+		<li><a class="active" href="logout.php">LOGOUT</a></li>
+	  </ul>	 
+	</nav>
+	
+	<main>
+		
+		<h1 style="text-align:center; color:#00cc00;">Thank You!!! You have successfully logged out of the system.</h1>
+		<h2>IMPORTANT INFORMATION</h2>
+		<h3 style="text-align:center;">It is a good thing to log yourself out of the system. Always remember to log yourself out once you are done using the website to save your identity from the cyber hackers. Thank you for using this website and See you again!!!</h3>
+		<div style="text-align:center; margin-bottom:30px;">
+			<button onclick="redirect()">Back to Home Page</button>
+		</div>
+	</main>
+	
+	<footer>
+		<p>The contents of this website are protected by <strong>copyright&copy; law.</strong> Copyright in this material resides with the Commonwealth of Australia or various other rights holders, as indicated. DP1 - Group 1 is the developer of this website as well the content provided in this website.</p>
+	</footer>
+</body>
+</html>
+
+<?php
+	session_start();
+	session_destroy();
 ?>
